a7425740c2429f38b6f67819f1e977c77d7a118b
might5 <56439279+might5@users.noreply.github.com>
2020-06-13 09:53:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/might5/luci-theme-argon1.git package/luci-theme-argon1
 git clone https://github.com/fw876/helloworld.git package/helloworld
+sed -i 's/luci-theme-bootstrap/luci-theme-argon1/g' ./feeds/luci/collections/luci/Makefile
